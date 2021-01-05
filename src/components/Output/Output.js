0296832f24e7f1c1b337db659ba8c70d2b5c1240
JakeRoyRandall <jakerandall.me>
2021-01-04 23:57:30 -0800

    updates made on edit screen

---
+++ src/components/Output/Output.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import './Output.css';
+
+const Output = props => {
+    
+    useEffect(() => {
+    }, [])
+
+    return (
+        <div>hello</div>
+    )
+}
+
+export default Output
